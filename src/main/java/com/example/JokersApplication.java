668096acee2378a15f6072e62ccb9c831139479d
宋伟 <610347922@qq.com>
2016-11-21 14:08:30 +0800

    1. 项目初始化

---
+++ src/main/java/com/example/JokersApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokersApplication.class, args);
+	}
+}
