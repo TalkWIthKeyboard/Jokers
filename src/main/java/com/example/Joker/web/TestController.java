6f1fa307a1bc90a910768c9f8aaa2837f5e504cc
宋伟 <610347922@qq.com>
2016-12-01 16:09:12 +0800

    1. debug

--- src/main/java/com/example/Joker/web/TestController.java
+++ src/main/java/com/example/Joker/web/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Objects;
@@ -16,7 +17,7 @@ import java.util.Objects;
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String index(
             HttpServletRequest request,
             Model model
